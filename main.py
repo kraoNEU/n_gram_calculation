979ff83a7a5b6a78b7a5452d2371df1fb6680f86
kraoNEU <chelamkuri.v@northeastern.edu>
2022-07-30 20:02:40 -0400

    added the code to work

--- main.py
+++ main.py
@@ -1,39 +1,92 @@
+import math
+
 import pandas as pd
-import numpy as np
 
 n_gram_string_list = []
 excel_2_gram_probabilities_list = []
 excel_3_gram_probabilities_list = []
 empty_2_gram_list = []
 empty_3_gram_list = []
+cleaned_input_string_list = []
 
 df_N_gram_2 = pd.read_csv("/Users/cvkrishnarao/Desktop/RA/n_gram_data_converted/n_gram_coca_x2w_utf_8.csv")
 df_N_gram_3 = pd.read_csv("/Users/cvkrishnarao/Desktop/RA/n_gram_data_converted/n_gram_coca_x3w_utf_8.csv")
 
-n_gram_value = 2
-test_string = "hi this is ratan sai rohith sexy boy"
-x = [test_string.lower() for word in test_string.split()][0]
-x = x.split()
 
-for i in range(len(x)):
-    y = x[i:i + n_gram_value]
-    if len(y) == n_gram_value:
-        n_gram_string_list.append(y)
+def cleanupString(uncleanedString):
+    """
+    :param uncleanedString: Uncleaned String with all special Characters and stuff
+    :return: cleaned string with no special characters
+    """
+    uncleanedString = uncleanedString.replace('*', '')
+    uncleanedString = uncleanedString.replace('(', '')
+    uncleanedString = uncleanedString.replace(')', '')
+    uncleanedString = uncleanedString.replace('-', '')
+    uncleanedString = uncleanedString.replace(',', '')
+    uncleanedString = uncleanedString.replace('.', '')
+    uncleanedString = uncleanedString.replace(':', '')
+    uncleanedString = uncleanedString.replace('?', '')
+    uncleanedString = uncleanedString.replace('!', '')
+    uncleanedString = uncleanedString.replace('\n', '')
+    uncleanedString = uncleanedString.replace('\t', '')
+    uncleanedString = uncleanedString.replace('-', '')
+    w = uncleanedString.split()
+    f = ""
+    for wrd in w:
+        wrd = wrd.lower()
+        f = f + " " + wrd
+    return f
 
-if n_gram_value == 2:
-    for i in n_gram_string_list:
-        query_dataframe_2_gram = df_N_gram_2.loc[(df_N_gram_2['Word_One'] == i[0]) & (df_N_gram_2['Word_Two'] == i[1])]
-        excel_2_gram_probabilities_list.append(query_dataframe_2_gram.values.tolist())
-    print(excel_2_gram_probabilities_list)
 
-elif n_gram_value == 3:
-    for i in n_gram_string_list:
-        query_dataframe_3_gram = df_N_gram_3.loc[(df_N_gram_3['Word_One'] == i[0]) & (df_N_gram_3['Word_Two'] == i[1]) & (df_N_gram_3['Word_Three'] == i[2])]
+def n_gram_list(input_Cleaned_string, n_gram_value):
+    """
+    :param input_Cleaned_string: Cleaned string without spaces or special characters
+    :param n_gram_value: value of n-gram split == 2 or 3, 4, 5
+    :return: Splitted work list according to the n_gram_values
+    """
+    split_string = input_Cleaned_string.split()
+    for string in range(len(split_string)):
+        y = split_string[string:string + n_gram_value]
+        if len(y) == n_gram_value:
+            n_gram_string_list.append(y)
+    return n_gram_string_list
 
-        if query_dataframe_3_gram is not None:
-            excel_3_gram_probabilities_list.append(query_dataframe_3_gram.values.tolist())
-        else:
-            # Need to append the List for No Query Result
-            pass
 
-    print(excel_3_gram_probabilities_list)
+def n_gram_probability_calculation(n_gram_value, n_gram):
+    """
+    Calculates the respective score for the n-gram dataset. n_gram_value == 2 or 3, 4, 5
+    n_gram = actual list of the splitted word count
+    returns: excel_2_gram_probabilities_list with the frequency count for the values and the probability
+    """
+    sum_frequencies = 0
+    count_n_gram = len(n_gram)
+    if n_gram_value == 2:
+        for query_string in n_gram:
+            query_dataframe_2_gram = df_N_gram_2.loc[
+                (df_N_gram_2['Word_One'] == query_string[0]) & (df_N_gram_2['Word_Two'] == query_string[1])]
+
+            values_frequency = query_dataframe_2_gram["Frequency"]
+            sum_frequencies += sum_frequencies + math.log(values_frequency)
+            excel_2_gram_probabilities_list.append(query_dataframe_2_gram.values.tolist())
+
+        mean_values = sum_frequencies / count_n_gram
+        print(mean_values)
+        return excel_2_gram_probabilities_list
+
+    elif n_gram_value == 3:
+        for query_string in n_gram:
+            query_dataframe_3_gram = df_N_gram_3.loc[
+                (df_N_gram_3['Word_One'] == query_string[0]) & (df_N_gram_3['Word_Two'] == query_string[1]) & (
+                        df_N_gram_3['Word_Three'] == query_string[2])]
+
+            if query_dataframe_3_gram is not None:
+                excel_3_gram_probabilities_list.append(query_dataframe_3_gram.values.tolist())
+            else:
+                # Need to append the List for No Query Result
+                pass
+
+
+x = cleanupString("Baby wants more pie.")
+gram_list = n_gram_list(x, 2)
+statements = n_gram_probability_calculation(2, gram_list)
+print(statements)
