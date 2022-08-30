b058ddf59902f69fdeb12ba6465a4c745b8da4c2
kraoNEU <chelamkuri.v@northeastern.edu>
2022-08-30 21:37:51 +0530

    Added the files and the function for multiple and single line for bi and tri gram

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from tri_gram_calculation import cleanupString_tri_gram, n_gram_list_tri_gram, n
 # df = pd.read_csv("/Users/cvkrishnarao/Desktop/n_gram_document_1_copy.csv")
 # list_n_gram = list(df["SET 4"])
 
+# Multiple Sentences
 # # Bi-gram calculation
 # for n_gram in list_n_gram:
 #     x = cleanupString_bi_gram(n_gram)
@@ -15,7 +16,7 @@ from tri_gram_calculation import cleanupString_tri_gram, n_gram_list_tri_gram, n
 #     n_gram_probability_mean = 0
 #     gram_list.clear()
 
-
+# Multiple Sentences
 # # Tri-gram calculation
 # for n_gram in list_n_gram:
 #     x = cleanupString_tri_gram(n_gram)
@@ -28,8 +29,16 @@ from tri_gram_calculation import cleanupString_tri_gram, n_gram_list_tri_gram, n
 #         n_gram_probability_mean = 0
 #         gram_list.clear()
 
-n_gram = "Lion said boo to  baby."
-x = cleanupString_tri_gram(n_gram)
-gram_list = n_gram_list_tri_gram(x, 3)
-frequency_Count, n_gram_probability_mean, n_gram_individual_probability = n_gram_probability_calculation_tri_gram(3, gram_list)
+# Single Tri-gram sentence
+# n_gram = "Smart bot helps clean."
+# x = cleanupString_tri_gram(n_gram)
+# gram_list = n_gram_list_tri_gram(x, 3)
+# frequency_Count, n_gram_probability_mean, n_gram_individual_probability = n_gram_probability_calculation_tri_gram(3, gram_list)
+# print(f"{frequency_Count}, {n_gram_individual_probability}, {n_gram_probability_mean}\n")
+
+# Single bi-gram sentence
+n_gram = "Tasty pie."
+x = cleanupString_bi_gram(n_gram)
+gram_list = n_gram_list_bi_gram(x, 2)
+frequency_Count, n_gram_probability_mean, n_gram_individual_probability = n_gram_probability_calculation_bi_gram(2, gram_list)
 print(f"{frequency_Count}, {n_gram_individual_probability}, {n_gram_probability_mean}\n")
